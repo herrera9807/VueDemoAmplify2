5fb88d8d4c0e8eec53736a566e33a3945ec5a15d
unknown <oherrera@exikhan.com.mx>
2020-09-23 11:17:13 -0500

    v3

--- src/views/Acceso.vue
+++ src/views/Acceso.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Acceso de Usuarios editado v2</h1>
+    <h1>Acceso de Usuarios editado v3</h1>
     <form @submit.prevent="ingresoUsuario({ email:$v.email.$model, password:$v.pass.$model })">
       <input
         type="email"
